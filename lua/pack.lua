6f53ed2972ddd533db646cb7237ca1c05717e88e
Leonardo Groos Lago <leonardog_rs@yahoo.com.br>
2026-08-18 06:50:57 -0300

    Revert changes, didnt like them

--- lua/pack.lua
+++ lua/pack.lua
@@ -15,12 +15,8 @@ vim.pack.add({
 
 require('mini.completion').setup({
 	lsp_completion = {
-		-- Adicionamos o parâmetro "base", que é o texto que você digitou (ex: "mov")
-		process_items = function(items, base)
-			local filtered_items = {}
-
+		process_items = function(items)
 			for _, item in ipairs(items) do
-				-- 1. Toda a sua lógica original de limpeza de dados (mantida idêntica)
 				if type(item.labelDetails) ~= 'table' then
 					item.labelDetails = nil
 				else
@@ -41,50 +37,11 @@ require('mini.completion').setup({
 				elseif type(item.documentation) ~= 'string' then
 					item.documentation = nil
 				end
-
-				-- 2. Nova lógica de filtragem:
-				-- Só adiciona o item na lista final se ele contiver o que você digitou
-				-- Usamos item.filterText ou item.label para fazer a busca
-				local text_to_check = item.filterText or item.label or ""
-				if string.find(text_to_check:lower(), base:lower(), 1, true) ~= nil then
-					table.insert(filtered_items, item)
-				end
 			end
-
-			-- Retorna apenas os itens que passaram no filtro
-			return filtered_items
+			return items
 		end,
 	},
 })
--- require('mini.completion').setup({
--- 	lsp_completion = {
--- 		process_items = function(items)
--- 			for _, item in ipairs(items) do
--- 				if type(item.labelDetails) ~= 'table' then
--- 					item.labelDetails = nil
--- 				else
--- 					if type(item.labelDetails.detail) ~= 'string' then
--- 						item.labelDetails.detail = nil
--- 					end
--- 					if type(item.labelDetails.description) ~= 'string' then
--- 						item.labelDetails.description = nil
--- 					end
--- 				end
--- 				if type(item.detail) ~= 'string' then
--- 					item.detail = nil
--- 				end
--- 				if type(item.documentation) == 'table' then
--- 					if type(item.documentation.value) ~= 'string' then
--- 						item.documentation.value = nil
--- 					end
--- 				elseif type(item.documentation) ~= 'string' then
--- 					item.documentation = nil
--- 				end
--- 			end
--- 			return items
--- 		end,
--- 	},
--- })
 require('mini.snippets').setup({
 	snippets = {
 		require('mini.snippets').gen_loader.from_lang(),
@@ -106,8 +63,7 @@ local servers = {
   "nixd",
   "jedi_language_server",
   "gopls",
-	"glsl_analyzer",
-  "asm_lsp"
+	"glsl_analyzer"
 }
 vim.lsp.config('clangd', {
   cmd = {"clangd", "--background-index", "--clang-tidy", "--header-insertion=never"}
